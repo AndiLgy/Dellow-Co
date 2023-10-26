de0e11ad11722631613851e299adc926ef3d82aa
AndiLgy <146346864+AndiLgy@users.noreply.github.com>
2023-10-26 20:55:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const day = 'monday';
+
+if (day === 'monday') {
+    console.log('Plan something');
+    console.log('Go shopping');
+}
+else if (day === 'tuesday') {
+    console.log('Prepare dinner');
+}
+else if (day === 'wednsday' || day === 'thursday') {
+    console.log('Record a teachinf video');
+}
+else.if(day ==== 'friday'){
+    console.log('Take a break');
+}
